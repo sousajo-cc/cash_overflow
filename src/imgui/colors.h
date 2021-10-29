a88600fd06a112060ae93f811b3e6616d460acb2
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-10-29 15:54:15 +0100

    sorted out includes

--- src/imgui/colors.h
+++ src/imgui/colors.h
@@ -1,6 +1,8 @@
 #ifndef MYPROJECT_COLORS_H
 #define MYPROJECT_COLORS_H
 
+#include <imgui.h>
+
 namespace colors {
 const ImVec4 Red{ .7F, .0F, .0F, 1.0F };
 const ImVec4 Green{ .0F, .8F, .0F, 1.0F };
