d4f93fc7037e37e28b62ccc3691f36f1c5cb6c78
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-10-29 13:31:18 +0100

    refactored mockup to use the raii wrapper

--- src/imgui/test.cpp
+++ src/imgui/test.cpp
@@ -2,9 +2,58 @@
 #include <imgui-SFML.h>
 
 #include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
+
+#include "colors.h"
+#include "imgui_raii_wrapper.h"
+
+void draw_table() {
+
+  using colors::Green;
+  using colors::Red;
+
+  irw::Windowzita mockup{"Mockup Table"};
+  irw::TabBar tab_bar{"TabBar"};
+  if (irw::TabItem tab_item{"Accounts & Categories"}) {
+    auto table = irw::Table::Builder{}.with_id("Table")
+      .with_number_of_columns(4)
+      .with_headers("Account/Category", "Remaining Budget", "Change", "Total")
+      .add_row("Assets", "", irw::Text{"$-778.32", Red}, "$29,754.72")
+      .add_row("|- Cash", "", irw::Text{"$-2.85", Red}, "$236.30")
+      .add_row("|- Checking Account", "", irw::Text{"$-634.99", Red}, "$7,327.01")
+      .add_row("|- Euro Account", "", irw::Text{"-120.00 EUR", Red}, "5,403.00 EUR")
+      .add_row("|- Investment Account", "", "0 CNY", "20,400.00 CNY")
+      .add_row("|- Savings Account", "", "$0.00", "$12,800.00")
+      .add_row("Liabilities", "", irw::Text{"$-200.00", Green}, "$11,400.00")
+      .add_row("|- Car Loan", "", irw::Text{"$-200.00", Green}, "$11,400.00")
+      .add_row("Incomes", "-", "$0.00", "$18,853.50")
+      .add_row("|- Other", "-", "$0.00", "$5,853.50")
+      .add_row("|- Salary", "-", "$0.00", "$13,000.00")
+      .add_row("Expenses", irw::Text{"$226.67 of $460.00", Green}, irw::Text{"$578.32", Red}, "$4,532.67")
+      .add_row("|- Bills", "-", irw::Text{"$30.00", Red}, "$3,092.00")
+      .add_row("|- Clothing", irw::Text{"$-40.48 of $100.00", Red}, irw::Text{"$140.48", Red}, "$949.48")
+      .add_row("|- Groceries", irw::Text{"$74.15 of $160.00", Green}, irw::Text{"$85.85", Red}, "$155.20")
+      .add_row("|- Leisure", irw::Text{"$193.00 of $200.00", Green}, irw::Text{"$7.00", Red}, "$21.00")
+      .add_row("|- Other", "-", irw::Text{"$314.99", Red}, "$314.99")
+      .build();
+  }
+  if (irw::TabItem tab_item{"Expenses"}) {
+    irw::Text("Manga");
+  }
+  if (irw::TabItem tab_item{"Incomes"}) {
+    irw::Text("My income is €100k (daily)");
+  }
+  if (irw::TabItem tab_item{"Transfers"}) {
+    irw::Text("I transfered everything to the manga store");
+  }
+  if (irw::TabItem tab_item{"Securities"}) {
+    irw::Text("NullPointerException");
+  }
+  if (irw::TabItem tab_item{"Schedule"}) {
+    irw::Text("Minha criatividade já se acabou");
+  }
+}
 
 int main()
 {
@@ -15,9 +64,6 @@ int main()
   sf::CircleShape shape{100.F};
   shape.setFillColor(sf::Color::Cyan);
   shape.setPosition(sf::Vector2f{500.0, 350.0});
-
-  const ImVec4 Red{.7F, .0F, .0F, 1.0F};
-  const ImVec4 Green{.0F, .8F, .0F, 1.0F};
 
   sf::Clock deltaClock;
   while (window.isOpen()) {
@@ -32,186 +78,7 @@ int main()
 
     ImGui::SFML::Update(window, deltaClock.restart());
 
-    ImGui::Begin("Mockup");
-    ImGui::BeginTabBar("TabBar");
-    if (ImGui::BeginTabItem("Accounts & Categories")) {
-      if (ImGui::BeginTable("Table", 4)) {
-        ImGui::TableSetupColumn("Account/Category");
-        ImGui::TableSetupColumn("Remaining Budget");
-        ImGui::TableSetupColumn("Change");
-        ImGui::TableSetupColumn("Total");
-        ImGui::TableHeadersRow();
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Assets");
-        ImGui::TableNextColumn();
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$-778.32");
-        ImGui::TableNextColumn();
-        ImGui::Text("$29,754.72");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Cash");
-        ImGui::TableNextColumn();
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$-2.85");
-        ImGui::TableNextColumn();
-        ImGui::Text("$236.30");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Checking Account");
-        ImGui::TableNextColumn();
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$-634.99");
-        ImGui::TableNextColumn();
-        ImGui::Text("$7,327.01");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Euro Account");
-        ImGui::TableNextColumn();
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "-120.00 EUR");
-        ImGui::TableNextColumn();
-        ImGui::Text("5,403.00 EUR");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Investment Account");
-        ImGui::TableNextColumn();
-        ImGui::TableNextColumn();
-        ImGui::Text("0 CNY");
-        ImGui::TableNextColumn();
-        ImGui::Text("20,400.00 CNY");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Savings Account");
-        ImGui::TableNextColumn();
-        ImGui::TableNextColumn();
-        ImGui::Text("$0.00");
-        ImGui::TableNextColumn();
-        ImGui::Text("$12,800.00");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Liabilities");
-        ImGui::TableNextColumn();
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Green, "$-200.00");
-        ImGui::TableNextColumn();
-        ImGui::Text("$11,400.00");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Car Loan");
-        ImGui::TableNextColumn();
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Green, "$-200.00");
-        ImGui::TableNextColumn();
-        ImGui::Text("$11,400.00");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Incomes");
-        ImGui::TableNextColumn();
-        ImGui::Text("-");
-        ImGui::TableNextColumn();
-        ImGui::Text("$0.00");
-        ImGui::TableNextColumn();
-        ImGui::Text("$18,853.50");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Other");
-        ImGui::TableNextColumn();
-        ImGui::Text("-");
-        ImGui::TableNextColumn();
-        ImGui::Text("$0.00");
-        ImGui::TableNextColumn();
-        ImGui::Text("$5,853.50");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Salary");
-        ImGui::TableNextColumn();
-        ImGui::Text("-");
-        ImGui::TableNextColumn();
-        ImGui::Text("$0.00");
-        ImGui::TableNextColumn();
-        ImGui::Text("$13,000.00");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Expenses");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Green, "$226.67 of $460.00");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$578.32");
-        ImGui::TableNextColumn();
-        ImGui::Text("$4,532.67");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Bills");
-        ImGui::TableNextColumn();
-        ImGui::Text("-");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$30.00");
-        ImGui::TableNextColumn();
-        ImGui::Text("$3,092.00");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Clothing");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$-40.48 of $100.00");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$140.48");
-        ImGui::TableNextColumn();
-        ImGui::Text("$949.48");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Groceries");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Green, "$74.15 of $160.00");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$85.85");
-        ImGui::TableNextColumn();
-        ImGui::Text("$155.20");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Leisure");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Green, "$193.00 of $200.00");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$7.00");
-        ImGui::TableNextColumn();
-        ImGui::Text("$21.00");
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("|- Other");
-        ImGui::TableNextColumn();
-        ImGui::Text("-");
-        ImGui::TableNextColumn();
-        ImGui::TextColored(Red, "$314.99");
-        ImGui::TableNextColumn();
-        ImGui::Text("$314.99");
-        ImGui::EndTable();
-      }
-      ImGui::EndTabItem();
-    }
-    if (ImGui::BeginTabItem("Expenses")) {
-      ImGui::Text("Manga");
-      ImGui::EndTabItem();
-    }
-    if (ImGui::BeginTabItem("Incomes")) {
-      ImGui::Text("My income is €100k (daily)");
-      ImGui::EndTabItem();
-    }
-    if (ImGui::BeginTabItem("Transfers")) {
-      ImGui::Text("I transfered everything to the manga store");
-      ImGui::EndTabItem();
-    }
-    if (ImGui::BeginTabItem("Securities")) {
-      ImGui::Text("NullPointerException");
-      ImGui::EndTabItem();
-    }
-    if (ImGui::BeginTabItem("Schedule")) {
-      ImGui::Text("Minha criatividade já se acabou");
-      ImGui::EndTabItem();
-    }
-    ImGui::EndTabBar();
-    ImGui::End();
+    draw_table();
 
     window.clear();
     window.draw(shape);
