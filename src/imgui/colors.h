644cf3f9fecf32417bb2adec0539bd49845985c3
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-10-29 13:29:53 +0100

    moved colors to a new file

---
+++ src/imgui/colors.h
@@ -0,0 +1,9 @@
+#ifndef MYPROJECT_COLORS_H
+#define MYPROJECT_COLORS_H
+
+namespace colors {
+const ImVec4 Red{ .7F, .0F, .0F, 1.0F };
+const ImVec4 Green{ .0F, .8F, .0F, 1.0F };
+}
+
+#endif//MYPROJECT_COLORS_H
