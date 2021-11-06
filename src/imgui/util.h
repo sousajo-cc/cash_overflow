b8ffa5e54cbdc9f1d44e20af9e7ad7b80e08600d
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-11-06 21:02:50 +0000

    concept instead of requires-requires

--- src/imgui/util.h
+++ src/imgui/util.h
@@ -5,13 +5,15 @@
 
 namespace cash_overflow::util {
 
+template<typename F, typename ReturnType, typename... ParamTypes>
+concept Callable = requires (F function, ReturnType result, ParamTypes... params) {
+  result = function(params...);
+};
+
 //WARNING: ignore this if you're starting to learn C++ now
 //applies a function to every element on a vector
 //refer to the tests for usage examples
-template<typename Out, typename In, typename F, template<typename> typename Container>
-  requires requires (Out output_element, In input_element, F function) {
-    output_element = function(input_element);
-  }
+template<typename Out, typename In, Callable<Out, In> F, template<typename> typename Container>
 Container<Out> map(Container<In> const& input, F f) {
   Container<Out> output;
   output.resize(input.size());
