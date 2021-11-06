5947592f19c3ac7e25e5705744a4d9ea5f6e8d65
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-11-06 21:22:16 +0000

    CTAD all the way. so beautiful

--- src/imgui/util.h
+++ src/imgui/util.h
@@ -2,19 +2,16 @@
 #define CASH_OVERFLOW_UTIL_H
 
 #include <algorithm>
+#include <concepts>
 
 namespace cash_overflow::util {
-
-template<typename F, typename ReturnType, typename... ParamTypes>
-concept Callable = requires (F function, ReturnType result, ParamTypes... params) {
-  result = function(params...);
-};
 
 //WARNING: ignore this if you're starting to learn C++ now
 //applies a function to every element on a vector
 //refer to the tests for usage examples
-template<typename Out, typename In, Callable<Out, In> F, template<typename> typename Container>
-Container<Out> map(Container<In> const& input, F f) {
+template<typename In, std::invocable<In> F, template<typename> typename Container>
+auto map(Container<In> const& input, F f) -> Container<decltype(f(input[0]))> {
+  using Out = decltype(f(input[0]));
   Container<Out> output;
   output.resize(input.size());
   std::transform(input.begin(), input.end(), output.begin(), f);
--- test/map_tests.cpp
+++ test/map_tests.cpp
@@ -7,13 +7,13 @@ using cash_overflow::util::map;
 TEST(MapTests, DoubleTheNumbersInVec) {
   std::vector<int> input{6, 42, -3};
   std::vector<long int> expected_output{12l, 84l, -6l};
-  auto output = map<long int>(input, [](int n) { return 2l*n; });
+  auto output = map(input, [](int n) { return 2l*n; });
   EXPECT_EQ(output, expected_output);
 }
 
 TEST(MapTests, VecElementsToString) {
   std::vector<int> input{6, 42, -3};
   std::vector<std::string> expected_output{"6", "42", "-3"};
-  auto output = map<std::string>(input, [](int n) { return std::to_string(n); });
+  auto output = map(input, [](int n) { return std::to_string(n); });
   EXPECT_EQ(output, expected_output);
 }
