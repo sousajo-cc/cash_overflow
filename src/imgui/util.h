550a91fad43edef1a88ac2afb19250646319b062
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-11-06 09:35:30 +0000

    added constraint

--- src/imgui/util.h
+++ src/imgui/util.h
@@ -6,6 +6,9 @@
 namespace cash_overflow::util {
 
 template<typename In, typename Out, typename F, template<typename> typename Container>
+  requires requires (In input_element, Out output_element, F function) {
+    output_element = function(input_element);
+  }
 Container<Out> map(Container<In> const& input, F f) {
   Container<Out> output;
   output.resize(input.size());
