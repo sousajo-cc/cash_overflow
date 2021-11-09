761534c7b49740954d34b210b306426344296340
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-11-09 10:31:13 +0000

    removed unused dependencies

--- conanfile.py
+++ conanfile.py
@@ -5,10 +5,7 @@ class CppStarterProject(ConanFile):
     name = "CppStarterProject"
     version = "0.1"
     requires = (
-        "catch2/2.13.7",
-        "docopt.cpp/0.6.2",
         "fmt/8.0.1",
-        "spdlog/1.9.2",
         "imgui-sfml/2.2@bincrafters/stable",
     )
     generators = "cmake", "gcc", "txt", "cmake_find_package"
