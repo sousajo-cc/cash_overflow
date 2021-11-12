b88a536477010a13e51f2e34a84ccc51afa1ef96
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-11-12 22:37:05 +0000

    making structs into proper classes

--- src/imgui/date.hpp
+++ src/imgui/date.hpp
@@ -8,36 +8,46 @@
 #include "error.hpp"
 #include "logger.hpp"
 
-struct Day
+class Day
 {
-  Day(int v) : value{ v } {}
+public:
+  explicit Day(int v) : value{ v } {}
   auto operator<=>(Day const &) const = default;
+  int getValue() const {
+    return value;
+  }
+private:
   int value{};
 };
 
-struct Year
+class Year
 {
-  Year(int v) : value{ v } {}
+public:
+  explicit Year(int v) : value{ v } {}
   auto operator<=>(Year const &) const = default;
+  int getValue() const {
+    return value;
+  }
   bool isLeapYear()
   {
     return value % 4 == 0 && !(value % 100 == 0 && (value % 400 != 0));
   }
-  int value{};
   Day getNumberOfDays(Year year)
   {
-      return year.isLeapYear() ? 366 : 365;
+    return Day{ year.isLeapYear() ? 366 : 365 };
   }
-
+private:
+  int value{};
 };
 
-
-
-struct Month
+class Month
 {
-  Month(int v) : value{ v } {}
+public:
+  explicit Month(int v) : value{ v } {}
   auto operator<=>(Month const &) const = default;
-  int value{};
+  int getValue() const {
+    return value;
+  }
   Day getNumberOfDays(Year year)
   {
     switch (value) {
@@ -45,7 +55,7 @@ struct Month
     case 4:
     case 6:
     case 9:
-      return 30;
+      return Day{ 30 };
     case 12:
     case 10:
     case 8:
@@ -53,14 +63,16 @@ struct Month
     case 5:
     case 3:
     case 1:
-      return 31;
+      return Day{ 31 };
     case 2: {
-      return year.isLeapYear() ? 29 : 28;
+      return Day{ year.isLeapYear() ? 29 : 28 };
     }
     default:
-      return 0;
+      return Day{ 0 };
     }
   }
+private:
+  int value{};
 };
 
 
@@ -68,29 +80,32 @@ class Date
 {
 public:
   using Error = cash_overflow::error::Error;
+  static tl::expected<Date, Error> create(int y, int m, int d) {
+    return create(Year{y}, Month{m}, Day{d}); //TODO: remove
+  }
   static tl::expected<Date, Error> create(Year y, Month m, Day d)
   {
-    if (y <= 1970) {
+    if (y < Year{ 1970 }) {
       return tl::make_unexpected("Year needs to be in the recent past (until 1970), if you want to go that way back get a time machine asshole.");
     }
-    if (m < 1 || m > 12) {
+    if (m < Month{ 1 } || m > Month{ 12 }) {
       return tl::make_unexpected("Month must be between one and twelve");
     }
 
-    if (d <= 0) {
+    if (d <= Day{ 0 }) {
       return tl::make_unexpected("Day is out of range for selected Month");
     }
 
-    if ((m == 11 || m == 4 || m == 6 || m == 9) && d > 30) {
+    if ((m == Month{ 11 } || m == Month{ 4 } || m == Month{ 6 } || m == Month{ 9 }) && d > Day{ 30 }) {
       return tl::make_unexpected("Day is out of range for selected Month");
     }
 
-    if (d > 31) {
+    if (d > Day{ 31 }) {
       return tl::make_unexpected("Day is out of range for selected Month");
     }
 
-    if (m == 2) {
-      Day lastDay = y.isLeapYear() ? 29 : 28;
+    if (m == Month{ 2 }) {
+      Day lastDay = Day{ y.isLeapYear() ? 29 : 28 };
       if (d > lastDay) {
         return tl::make_unexpected("Day is out of range for selected Month");
       }
@@ -99,7 +114,7 @@ public:
   }
   std::string toString()
   {
-    return fmt::format("{}-{}-{}", year.value, month.value, day.value);
+    return fmt::format("{}-{}-{}", year.getValue(), month.getValue(), day.getValue());
   }
   friend std::ostream &operator<<(std::ostream &os, Date date)
   {
@@ -109,7 +124,7 @@ public:
   auto operator<=>(Date const &) const = default;
   tl::expected<Date, Error> operator+(Year y)
   {
-    return Date::create(year.value + y.value, month, day);
+    return Date::create(Year{ year.getValue() + y.getValue() }, month, day);
   }
   tl::expected<Date, Error> operator+(Month m)
   {
@@ -117,22 +132,22 @@ public:
 
     auto &LOGGER = cashoverflow::logging::Logger::log(LoggingLevel::ERR);
     
-    if (month.value + m.value < 12) {
-      return Date::create(year, month.value + m.value, day);
+    if (month.getValue() + m.getValue() < 12) {
+      return Date::create(year, Month{ month.getValue() + m.getValue() }, day);
     }
-    int yearsToAdd = m.value / 12;
+    int yearsToAdd = m.getValue() / 12;
     LOGGER.write(yearsToAdd, LoggingLevel::DBG);
 
-    int remainingMonths = m.value % 12;
+    int remainingMonths = m.getValue() % 12;
     LOGGER.write(remainingMonths, LoggingLevel::DBG);
 
-    auto monthsToAdd = month.value + remainingMonths;
+    auto monthsToAdd = month.getValue() + remainingMonths;
     LOGGER.write(monthsToAdd, LoggingLevel::DBG);
 
     if (monthsToAdd > 12) {
-      return Date::create(year.value + yearsToAdd + 1, monthsToAdd - 12, day);
+      return Date::create(Year{ year.getValue() + yearsToAdd + 1 }, Month{ monthsToAdd - 12 }, day);
     } else {
-      return Date::create(year.value + yearsToAdd, monthsToAdd, day);
+      return Date::create(Year{ year.getValue() + yearsToAdd }, Month{ monthsToAdd }, day);
     }
   }
 
@@ -141,31 +156,27 @@ public:
     auto m_ = month;
     auto y_ = year;
     auto d_ = day;
-    while (d.value > m_.getNumberOfDays(y_)){
-      d.value -= m_.getNumberOfDays(y_).value;
-      if (m_.value == 12) {
-        m_.value = 1;
-        ++y_.value;
+    while (d > m_.getNumberOfDays(y_)){
+      d = Day{ d.getValue() - m_.getNumberOfDays(y_).getValue() };
+      if (m_.getValue() == 12) {
+        m_ = Month{ 1 };
+        y_ = Year{ y_.getValue() + 1 };
       } else {
-        ++m_.value;
+        m_ = Month{ m_.getValue() + 1 };
       }
     }
-    d_.value += d.value;
-    if (d_.value > m_.getNumberOfDays(y_)) {
-      d_.value -= m_.getNumberOfDays(y_).value;
-      m_.value++;
+    d_ = Day{ d_.getValue() + d.getValue() };
+    if (d_ > m_.getNumberOfDays(y_)) {
+      d_ = Day{ d_.getValue() - m_.getNumberOfDays(y_).getValue() };
+      m_ = Month{ m_.getValue() + 1 };
     }
     return Date::create(y_, m_, d_);
   }
-
 private:
-  Date(Year y, Month m, Day d) : year(y), month(m), day(d)
-  {}
+  Date(Year y, Month m, Day d) : year(y), month(m), day(d) {}
   Year year;
   Month month;
   Day day;
 };
-
-// Date::create()
 
 #endif// CASH_OVERFLOW_DATE_HPP
