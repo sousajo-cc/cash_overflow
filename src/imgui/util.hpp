4a43fc994b6b8db8666d2d114ea8f94a7cf1c5a0
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-11-12 20:55:43 +0000

    printable expected

--- src/imgui/util.hpp
+++ src/imgui/util.hpp
@@ -1,6 +1,8 @@
 #ifndef CASH_OVERFLOW_UTIL_H
 #define CASH_OVERFLOW_UTIL_H
 
+#include <tl/expected.hpp>
+
 #include <algorithm>
 #include <concepts>
 
@@ -17,6 +19,23 @@ auto map(Container<In> const &input, F f) -> Container<decltype(f(input[0]))>
   output.resize(input.size());
   std::transform(input.begin(), input.end(), output.begin(), f);
   return output;
+}
+
+// WARNING: ignore this if you're starting to learn C++ now
+// makes tl::expected printable if the value type and error type are printable
+// great for making sense of GTest result
+template<typename V, typename E>
+requires requires (V v, E e) {
+  std::cout << v;
+  std::cout << e;
+}
+std::ostream& operator<<(std::ostream& os, tl::expected<V, E> const& expected) {
+  if (expected) {
+    os << expected.value();
+  } else {
+    os << expected.error();
+  }
+  return os;
 }
 
 }// namespace cash_overflow::utils
