45726abf315fcf8d9ef7b7ed474ac8e6868a8f49
sousajo <sousajo@pop-os.localdomain>
2021-11-19 15:39:01 +0000

    filehandler: remove stream as member

--- src/imgui/fileHandler.hpp
+++ src/imgui/fileHandler.hpp
@@ -6,37 +6,55 @@
 #include <string>
 
 namespace cashoverflow::utils {
-struct FileHandler
+
+class IFileHandler {
+  public:
+    virtual std::string read() const = 0;
+    virtual unsigned int read(char *buffer, int size) = 0;
+    virtual std::string getFileName() const = 0;
+    virtual ~IFileHandler() = default;
+};
+class FileHandler: public IFileHandler
 {
-  explicit FileHandler(std::string fileName_) : fileName(std::move(fileName_))
-  {
-    
-  };
-  FileHandler(FileHandler const &) = default;
-  FileHandler &operator=(FileHandler const &) = default;
-  FileHandler(FileHandler &&) = default;
-  FileHandler &operator=(FileHandler &&) = default;
-  ~FileHandler() = default;
-  // {
-  //   if (fileStream.is_open()) {
-  //     fileStream.close();
-  //   }
-  // }
-  void open() {
-    fileStream.open(fileName.c_str(), std::ios_base::in);
+  public:
+    explicit FileHandler(std::string fileName_) : fileName(std::move(fileName_))
+    {
+      fileStream.open(fileName.c_str(), std::ios_base::in);
       if (!fileStream.is_open()) {
         throw std::runtime_error("could not open the file.");
       }
-  }
-  std::string read() const
-  {
-    std::ostringstream os;
-    os << fileStream.rdbuf();
-    return os.str();
-  };
+    };
+    FileHandler(FileHandler const &) = delete;
+    FileHandler &operator=(FileHandler const &) = delete;
+    FileHandler(FileHandler &&) = delete;
+    FileHandler &operator=(FileHandler &&) = delete;
+    ~FileHandler()
+    {
+      if (fileStream.is_open()) {
+        fileStream.close();
+      }
+    }
+    
+    std::string read() const override
+    {
+      std::ostringstream os;
+      os << fileStream.rdbuf();
+      std::cout << os.str() << std::endl;
+      return os.str();
+    }
 
-  std::string fileName;
-  std::fstream fileStream;
+    unsigned int read(char *buffer, int size) override {
+      fileStream.read(buffer, size);
+      return static_cast<unsigned int>(fileStream.gcount()); 
+    }
+
+    std::string getFileName() const override {
+      return fileName;
+    }
+
+  private:  
+    std::string fileName;
+    std::fstream fileStream;
 };
 }// namespace cashoverflow::utils
 
--- src/imgui/user.hpp
+++ src/imgui/user.hpp
@@ -3,9 +3,9 @@
 #include <string>
 #include <vector>
 #include <ranges>
-#include "account.hpp" // card class
+//#include "account.hpp" // card class
 #include "money.hpp" // money class
-// #include "db.hpp" // db class (save and load users)
+#include "db.hpp" // db class (save and load users)
 
 namespace cash_overflow::user
 {
@@ -15,15 +15,21 @@ namespace cash_overflow::user
         std::string fullName;
         int userId;
         int age;
-        std::vector<account::Account> accounts;
-        money::Money money;
+        //std::vector<account::Account> accounts;
+        //money::Money money;
+        std::unique_ptr<cash_overflow::db::Db> db; 
     public:
         User(std::string _fullName, int _age): fullName{_fullName}, age{_age} {}; //test here
         [[nodiscard]] std::string getFullName() const { return fullName; }
         [[nodiscard]] int getAge() const { return age; }
-
+        void addNewUser(std::string userName, std::string passWord)
+        {
+            if(db->read(userName)) {
+                db->write(userName, passWord);
+            }
+        }
         void addNewAccount(unsigned int cardNumber) {
-             account::Account::create(cardNumber).value().deposit(90);
+             //account::Account::create(cardNumber).value().deposit(90);
         }
         void deleteAccount(unsigned int cardNumber) {
             // menu with user accounts
