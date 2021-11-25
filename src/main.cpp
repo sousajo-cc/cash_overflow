08cfd08773a83988a2885c44b253ca30b95845ad
sousajo <sousajo@pop-os.localdomain>
2021-11-25 12:17:47 +0000

    main: open pr

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,6 @@ int main(int argc, const char **argv)
   }
 
 
-  // Use the default logger (stdout, multi-threaded, colored)
   spdlog::info("Hello, {}!", "World");
 
   fmt::print("Hello, from {}\n", "{fmt}");
