1af44bbe9ba361b312fce348ad2fb36dbc9d6c80
Luiz Chagas Jardim <36336482+luizchagasjardim@users.noreply.github.com>
2021-12-17 08:13:20 -0300

    Money Class (#59)
    * money class
    
    * appeasing clang format
    
    Co-authored-by: Luiz Jardim <CTW00767@criticaltechworks.com>

---
+++ src/imgui/money.hpp
@@ -0,0 +1,44 @@
+#ifndef CASH_OVERFLOW_MONEY_HPP
+#define CASH_OVERFLOW_MONEY_HPP
+
+#include <iostream>
+
+namespace cash_overflow::money {
+
+class Money
+{
+public:
+  explicit Money(double a);
+  constexpr explicit Money(int currency, int cents) : amount{ 100 * currency + cents } {}
+  friend std::ostream &operator<<(std::ostream &os, Money money);
+  [[nodiscard]] constexpr auto operator<=>(Money const &) const = default;
+  constexpr auto operator+(Money const other) const
+  {
+    return Money{ 0, amount + other.amount };
+  }
+  constexpr auto operator-(Money const other) const
+  {
+    return Money{ 0, amount - other.amount };
+  }
+  constexpr auto operator-() const
+  {
+    return Money{ 0, -amount };
+  }
+  constexpr auto &operator+=(Money const &other)
+  {
+    amount += other.amount;
+    return *this;
+  }
+  constexpr auto &operator-=(Money const &other)
+  {
+    amount -= other.amount;
+    return *this;
+  }
+
+private:
+  int amount;
+};
+
+}// namespace cash_overflow::money
+
+#endif// CASH_OVERFLOW_MONEY_HPP
